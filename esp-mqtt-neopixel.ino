236fe645a7e340fa13a63cff2441cf1935fe650e
Edgar Toll <edjopato@gmail.com>
2019-06-26 14:47:14 +0200

    refactor: use EspMQTTClient
    simplifies code

--- esp-mqtt-neopixel.ino
+++ esp-mqtt-neopixel.ino
@@ -8,16 +8,22 @@
 //   a LOGIC-LEVEL CONVERTER on the data line is STRONGLY RECOMMENDED.
 // (Skipping these may work OK on your workbench but can fail in the field)
 
-#include <ESP8266WiFi.h>
-#include <PubSubClient.h>
 #include <Adafruit_NeoPixel.h>
-
-const char* ssid = "";
-const char* password = "";
-const char* mqtt_server = "";
-const bool mqtt_retained = true;
+#include <EspMQTTClient.h>
 
 #define CLIENT_NAME "espNeopixel"
+
+EspMQTTClient client(
+  "WifiSSID",
+  "WifiPassword",
+  "192.168.1.100",  // MQTT Broker server ip
+  "MQTTUsername",   // Can be omitted if not needed
+  "MQTTPassword",   // Can be omitted if not needed
+  CLIENT_NAME,     // Client name that uniquely identify your device
+  1883              // The MQTT port, default to 1883. this line can be omitted
+);
+
+const bool mqtt_retained = true;
 
 #define BASIC_TOPIC CLIENT_NAME "/"
 #define BASIC_TOPIC_SET BASIC_TOPIC "set/"
@@ -31,9 +37,6 @@ const int LED_PIN = 13;
 //const int LED_COUNT = 41;
 const int LED_COUNT = (60 * 5) - 41;
 
-WiFiClient espClient;
-PubSubClient client(espClient);
-
 // Declare our NeoPixel strip object:
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 // Argument 1 = Number of pixels in NeoPixel strip
@@ -45,106 +48,11 @@ Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 //   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
 
-uint16_t hue = 21845; // green
-uint8_t sat = 255;
+uint16_t hue = 120; // green
+uint8_t sat = 100;
 uint8_t bri = 10;
 boolean on = true;
 int x = 0;
-
-void setup_wifi() {
-
-  delay(10);
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  randomSeed(micros());
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-}
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  String payloadString = "";
-  for (unsigned int i = 0; i < length; i++) {
-    payloadString += (char)payload[i];
-  }
-  Serial.print(payloadString.length());
-  Serial.print(" ");
-  Serial.print(payloadString);
-
-  if (strcmp(topic, BASIC_TOPIC_SET "hue") == 0) {
-    hue = strtol(payloadString.c_str(), 0, 10) * 182; // move 360 to 2^16
-    client.publish(BASIC_TOPIC_STATUS "hue", payloadString.c_str(), mqtt_retained);
-  } else if (strcmp(topic, BASIC_TOPIC_SET "sat") == 0) {
-    sat = strtol(payloadString.c_str(), 0, 10) * 2.55;
-    client.publish(BASIC_TOPIC_STATUS "sat", payloadString.c_str(), mqtt_retained);
-  } else if (strcmp(topic, BASIC_TOPIC_SET "bri") == 0) {
-    bri = strtol(payloadString.c_str(), 0, 10);
-    client.publish(BASIC_TOPIC_STATUS "bri", payloadString.c_str(), mqtt_retained);
-  } else if (strcmp(topic, BASIC_TOPIC_SET "on") == 0) {
-    on = payloadString != "0";
-    client.publish(BASIC_TOPIC_STATUS "on", payloadString.c_str(), mqtt_retained);
-  } else {
-    // what?
-  }
-
-  x = 0;
-
-  Serial.println();
-}
-
-void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-
-    String clientName = CLIENT_NAME;
-    clientName += "-";
-    clientName += macAsString();
-
-    // Attempt to connect
-    if (client.connect(clientName.c_str(), BASIC_TOPIC "connected", 0, mqtt_retained, "0")) {
-      Serial.println("connected");
-      // Once connected, publish an announcement...
-      client.publish(BASIC_TOPIC "connected", "2", mqtt_retained);
-      // ... and resubscribe
-      client.subscribe(BASIC_TOPIC_SET "hue");
-      client.subscribe(BASIC_TOPIC_SET "sat");
-      client.subscribe(BASIC_TOPIC_SET "bri");
-      client.subscribe(BASIC_TOPIC_SET "on");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
-}
-
-String macAsString() {
-  uint8_t mac[6];
-  WiFi.macAddress(mac);
-  String result;
-  for (int i = 3; i < 6; ++i) {
-    result += String(mac[i], 16);
-  }
-  return result;
-}
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
@@ -155,19 +63,59 @@ void setup() {
   strip.show();            // Turn OFF all pixels ASAP
   //strip.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
 
-  WiFi.hostname(CLIENT_NAME);
-  setup_wifi();
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
+  // Optional functionnalities of EspMQTTClient :
+  client.enableDebuggingMessages(); // Enable debugging messages sent to serial output
+  client.enableLastWillMessage(BASIC_TOPIC "connected", "0", mqtt_retained);  // You can activate the retain flag by setting the third parameter to true
+}
+
+void onConnectionEstablished() {
+  client.subscribe(BASIC_TOPIC_SET "hue", [](const String & payload) {
+    int newHue = strtol(payload.c_str(), 0, 10);
+    if (hue != newHue) {
+      hue = newHue;
+      x = 0;
+      client.publish(BASIC_TOPIC_STATUS "hue", payload, mqtt_retained);
+    }
+  });
+
+  client.subscribe(BASIC_TOPIC_SET "sat", [](const String & payload) {
+    int newSat = strtol(payload.c_str(), 0, 10);
+    if (sat != newSat) {
+      sat = newSat;
+      x = 0;
+      client.publish(BASIC_TOPIC_STATUS "sat", payload, mqtt_retained);
+    }
+  });
+
+  client.subscribe(BASIC_TOPIC_SET "bri", [](const String & payload) {
+    int newBri = strtol(payload.c_str(), 0, 10);
+    if (bri != newBri) {
+      bri = newBri;
+      x = 0;
+      client.publish(BASIC_TOPIC_STATUS "bri", payload, mqtt_retained);
+    }
+  });
+
+  client.subscribe(BASIC_TOPIC_SET "on", [](const String & payload) {
+    boolean newOn = payload != "0";
+    if (on != newOn) {
+      on = newOn;
+      x = 0;
+      client.publish(BASIC_TOPIC_STATUS "on", payload, mqtt_retained);
+    }
+  });
+
+  client.publish(BASIC_TOPIC "connected", "2", mqtt_retained);
+  client.publish(BASIC_TOPIC_STATUS "hue", String(hue), mqtt_retained);
+  client.publish(BASIC_TOPIC_STATUS "sat", String(sat), mqtt_retained);
+  client.publish(BASIC_TOPIC_STATUS "bri", String(bri), mqtt_retained);
+  client.publish(BASIC_TOPIC_STATUS "on", on ? "1" : "0", mqtt_retained);
+
+  digitalWrite(LED_BUILTIN, HIGH);
 }
 
 int lastX = x;
 void loop() {
-  if (!client.connected()) {
-    digitalWrite(LED_BUILTIN, LOW);
-    reconnect();
-    digitalWrite(LED_BUILTIN, HIGH);
-  }
   client.loop();
 
   //stepInterlaced();
@@ -182,12 +130,20 @@ void loop() {
   delay(10);
 }
 
+void setHsv(int pixel, int hue, int sat, int bri, int on) {
+  // move hue from 360 to 2^16
+  // move sat from 100 to 256
+
+  // Set pixel's color (in RAM)
+  strip.setPixelColor(pixel, strip.ColorHSV(hue * 182, sat * 2.55, bri * on));
+}
+
 void stepInterlaced() {
   if (x >= strip.numPixels()) {
     return;
   }
 
-  strip.setPixelColor(x, strip.ColorHSV(hue, sat, bri * on)); //  Set pixel's color (in RAM)
+  setHsv(x, hue, sat, bri, on);
 
   x += 2;
   if (x >= strip.numPixels() && x % 2 == 0) {
@@ -201,7 +157,7 @@ void stepMulti() {
   }
 
   for (int i = x; i < strip.numPixels(); i += 10) {
-    strip.setPixelColor(i, strip.ColorHSV(hue, sat, bri * on));
+    setHsv(i, hue, sat, bri, on);
   }
 
   x++;
